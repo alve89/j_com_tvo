d9bd8e30a33c47a2f01e56edb7e388c6f1c77d71
alve89 <devel@stefan-herzog.com>
2021-09-06 09:19:29 +0200

    Changing update server to GitHub

--- site/models/tvo.php
+++ site/models/tvo.php
@@ -11,12 +11,11 @@
 defined('_JEXEC') or die('Restricted access');
 
 /**
- * HelloWorld Model
+ * Tvo Model
  *
  * @since  0.0.1
  */
-class TvoModelTvo extends JModelItem
-{
+class TvoModelTvo extends JModelItem {
 	/**
 	 * @var string message
 	 */
@@ -24,16 +23,29 @@ class TvoModelTvo extends JModelItem
 
 	/**
 	 * Get the message
-         *
+   *
 	 * @return  string  The message to be displayed to the user
 	 */
-	public function getMsg()
-	{
-		if (!isset($this->message))
-		{
-			$this->message = 'Hello World from model!';
-		}
+	public function getMsg() {
+		if (!isset($this->message)) {
+			//$this->message = 'Hello World from model!';
 
+			// Check if an option (set in /views/tvo/tmpl/default.xml => fields) was passed in the URL (e.g. ?option=2) or rather if this option was set in administrator => menu item => COM_HELLOWORLD_HELLOWORLD_FIELD_GREETING_LABEL
+			$jinput = JFactory::getApplication()->input;
+			$id     = $jinput->get('id', 1, 'INT');
+
+			switch ($id) {
+				case 2:
+					$this->message = 'Good bye World!';
+					break;
+				default:
+				case 1:
+					$this->message = 'Hello World!';
+					break;
+			}
+		}
 		return $this->message;
 	}
+
+
 }
